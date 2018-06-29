017a7d3e7016adddf54b1a89da56c2fc6feee420
cnsuer <842393459@qq.com>
2018-06-29 19:48:50 +0800

    添加

--- CryptoEthereumSwift/Source/Crypto.swift
+++ CryptoEthereumSwift/Source/Crypto.swift
@@ -8,6 +8,31 @@ public enum CryptoEthereumSwiftError: Error {
 
 /// Helper class for cryptographic algorithms.
 public final class Crypto {
+	
+	public static func sha256(_ data: Data) -> Data {
+		return CryptoHash.sha256(data)
+	}
+	/// Hashes data with SHA256 twice
+	///
+	/// - Parameter data: data to be hashed
+	/// - Returns: hash
+	public static func doubleSHA256(_ data: Data) -> Data {
+		return CryptoHash.sha256(CryptoHash.sha256(data))
+	}
+	
+	public static func ripemd160(_ data: Data) -> Data {
+		return CryptoHash.ripemd160(data)
+	}
+	
+	/// Returns 160-bit hash of the data
+	///
+	/// - Parameter data: data to be hashed
+	/// - Returns: hash
+	public static func hash160(_ data: Data) -> Data {
+		return CryptoHash.ripemd160(CryptoHash.sha256(data))
+	}
+	
+	
     /// Produces "hash-based message authentication code" that can be used to verify data integrity and authenticity.
     /// Hash is 512-bit length (64 bytes)
     ///
@@ -28,23 +53,7 @@ public final class Crypto {
     public static func PBKDF2SHA512(_ password: Data, salt: Data) -> Data {
         return PKCS5.pbkdf2(password, salt: salt, iterations: 2048, keyLength: 64)
     }
-    
-    /// Returns 160-bit hash of the data
-    ///
-    /// - Parameter data: data to be hashed
-    /// - Returns: hash
-    public static func hash160(_ data: Data) -> Data {
-        return CryptoHash.ripemd160(CryptoHash.sha256(data))
-    }
-    
-    /// Hashes data with SHA256 twice
-    ///
-    /// - Parameter data: data to be hashed
-    /// - Returns: hash
-    public static func doubleSHA256(_ data: Data) -> Data {
-        return CryptoHash.sha256(CryptoHash.sha256(data))
-    }
-    
+	
     /// Returns SHA3 256-bit (32-byte) hash of the data
     ///
     /// - Parameter data: data to be hashed
@@ -70,14 +79,36 @@ public final class Crypto {
     ///   - privateKey: serialized private key based on secp256k1 algorithm
     /// - Returns: 65-byte signature of the hash data
     /// - Throws: EthereumKitError.failedToSign in case private key was invalid
-    public static func sign(_ hash: Data, privateKey: Data) throws -> Data {
+    public static func signEth(_ hash: Data, privateKey: Data) throws -> Data {
         let encrypter = EllipticCurveEncrypterSecp256k1()
         guard var signatureInInternalFormat = encrypter.sign(hash: hash, privateKey: privateKey) else {
             throw CryptoEthereumSwiftError.failedToSign
         }
         return encrypter.export(signature: &signatureInInternalFormat)
     }
-    
+	
+	public static func signBtc(_ data: Data, privateKey: Data) throws -> Data {
+		let ctx = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN))!
+		defer { secp256k1_context_destroy(ctx) }
+		
+		let signature = UnsafeMutablePointer<secp256k1_ecdsa_signature>.allocate(capacity: 1)
+		defer { signature.deallocate(capacity: 1) }
+		let status = data.withUnsafeBytes { (ptr: UnsafePointer<UInt8>) in
+			privateKey.withUnsafeBytes { secp256k1_ecdsa_sign(ctx, signature, ptr, $0, nil, nil) }
+		}
+		guard status == 1 else { throw CryptoError.signFailed }
+		
+		let normalizedsig = UnsafeMutablePointer<secp256k1_ecdsa_signature>.allocate(capacity: 1)
+		defer { normalizedsig.deallocate(capacity: 1) }
+		secp256k1_ecdsa_signature_normalize(ctx, normalizedsig, signature)
+		
+		var length: size_t = 128
+		var der = Data(count: length)
+		guard der.withUnsafeMutableBytes({ return secp256k1_ecdsa_signature_serialize_der(ctx, $0, &length, normalizedsig) }) == 1 else { throw CryptoError.noEnoughSpace }
+		der.count = length
+		
+		return der
+	}
     /// Validates a signature of a hash with publicKey. If valid, it guarantees that the hash was signed by the
     /// publicKey's private key.
     ///
